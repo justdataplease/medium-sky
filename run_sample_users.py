3d938aa41dd71cece6df58f3d51480665b96ee4b
Jay <iasonas.tr@gmail.com>
2023-03-29 14:01:29 +0300

    added sample users generator

---
+++ run_sample_users.py
@@ -0,0 +1,9 @@
+import subprocess
+
+usernames = ['andre-ye', 'umairh', 'frank-andrade', 'lakshmanok', 'BernieSanders', 'nikoskafritsas', 'dima806', 'anne.bonfert', 'ngwaifoong92', 'coachtony',
+             'justdataplease', 'benjaminsledge', 'datasculptor', 'kozyrkov', 'dariusforoux', 'christianlauer90', 'ev', 'barackobama', 'profgalloway']
+
+for username in usernames:
+    command = f'python kgraph.py -u={username} -l=30'
+    subprocess.run(command, shell=True)
+    print(f"finished with {username}")
